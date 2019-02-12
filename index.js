ec35066c18030469a2d56f07f4888897342b58c0
Aaron Turner <aaron@aaronthedev.com>
2019-02-12 13:49:09 -0800

    Got the benchmark injection working

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const upload = multer(); // for parsing multipart/form-data
 const fetch = require('node-fetch');
 const { URLSearchParams } = require('url');
 
+// HTML Parser
+const cheerio = require('cheerio');
+
 app.use(cors());
 
 // Middleware
@@ -39,7 +42,34 @@ app.use((req, res, next) => {
   setHeaders: function (res, path, stat) {
     addAmpHeaders(staticRequest, res);
   }
-}));
+  })
+);
+
+// Serve amp page with the benchmarking script injected
+app.get('/benchmark', (req, res) => {
+  const asyncTask = async () => {
+
+    // Get the url for the query parameter
+    const pageUrl = req.query.page;
+
+    if (!pageUrl) {
+      res.status(400).send('No Page in query!');
+      return;
+    }
+
+    const response = await fetch(pageUrl)
+      .then(res => res.text());
+
+    // Inject our script into the bottom of the body
+    const $ = cheerio.load(response);
+    $('body').append('<script>console.log("Benchmark Injected!")</script>')
+
+    const injectedPage = $.html();
+
+    res.status(200).send(injectedPage);
+  };
+  asyncTask();
+});
 
 const port = 8000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
